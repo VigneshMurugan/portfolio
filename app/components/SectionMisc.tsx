a08c6b970f414d299515e598a3cd1eb485129449
Vignesh Murugan <vignesh328@gmail.com>
2023-12-02 17:18:41 +0530

    Removed irrelevant content

--- app/components/SectionMisc.tsx
+++ app/components/SectionMisc.tsx
@@ -7,36 +7,11 @@ export const SectionMisc = () => {
             Miscellaneous <span className="ml-2">🧩</span>
           </h2>
           <p>
-            Living in San Diego you can find me and the family at the beach!
-            Otherwise there's a good chance I am hauling ass along the coast on
-            my motorcycle 🏍️💨.
+            Living in Coimbatore/Bengaluru
           </p>
         </div>
         <div className="flex flex-shrink-0 flex-wrap gap-4">
-          <img
-            alt="Me, my son, and our dog at the beach"
-            className="aspect-square max-w-[47%] flex-grow"
-            height="auto"
-            loading="lazy"
-            src="https://s3-us-west-1.amazonaws.com/mattscholta/instagram/05.jpg"
-            width={200}
-          />
-          <img
-            alt="My 2014 Triumph Thruxton 900 in front of a Lifeguard tower"
-            className="aspect-square max-w-[47%] flex-grow"
-            height="auto"
-            loading="lazy"
-            src="https://s3-us-west-1.amazonaws.com/mattscholta/instagram/07.jpg"
-            width={200}
-          />
-          <img
-            alt="My 2014 Triumph Thruxton 900 in front some cool reeds"
-            className="aspect-square flex-grow"
-            height="auto"
-            loading="lazy"
-            src="https://s3-us-west-1.amazonaws.com/mattscholta/instagram/06.jpg"
-            width={200}
-          />
+        
         </div>
       </div>
     </section>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -47,7 +47,7 @@ export default function () {
               height="auto"
               loading="eager"
               onClick={onClick}
-              src="/images/assets/matt-scaled.webp"
+              src="/images/assets/vignesh-murugan.webp"
               width="auto"
             />
           </div>
@@ -55,9 +55,7 @@ export default function () {
       </div>
       <section className="border-0 border-b border-t border-solid border-color-border bg-color-background-light px-8 py-20 text-color-background-dark md:px-0">
         <blockquote className="mx-auto my-20 max-w-4xl text-2xl font-normal md:my-40 md:text-3xl">
-          Passionate about <b>quality code</b> written <b>for humans</b>,
-          unlocking <b>developer productivity</b>, and creating a delightful{" "}
-          <b>user experience</b>.
+      
         </blockquote>
       </section>
       <SectionAmbitions />
